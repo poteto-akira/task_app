a548430a42bba1236c059bbd4a446f3d85ed38af
poteto-akira <poteto.akira@gmail.com>
2018-11-04 13:30:41 +0900

    edit validate: 何か文字を入力しないと保存できない。DB制約をかけた。

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_052906) do
+ActiveRecord::Schema.define(version: 2018_11_04_042506) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "content"
     t.time "deadline_at"
     t.datetime "created_at", null: false

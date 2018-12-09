bb367cb72289e629a90f4d20109fe7707b8790e7
poteto-akira <poteto.akira@gmail.com>
2018-12-09 14:47:01 +0900

    refactor: delete comment

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,52 @@
+class UserController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      redirect_to("/", notice: "ユーザー登録が完了しました")
+    else
+      render("new")
+    end
+
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    # update_attributesメソッドは属性のハッシュを受け取り、成功時には更新と保存を続けて同時に行う
+    if @user.update_attributes(user_params)
+      redirect_to("/", notice: "ユーザー情報を変更しました")
+    else
+      render"edit"
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to("/", notice: "ユーザーを削除しました")
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    redirect_to("/") unless current_user?(@user) # session_helper
+  end
+end
